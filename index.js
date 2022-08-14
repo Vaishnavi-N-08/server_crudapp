4f2fa43580fa5a06f2fab95ac23e7a9493d3e23f
Vaishnavi-N-08 <vaishnavinarkhede8@gmail.com>
2022-08-14 18:27:54 +0530

    .

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 import express from 'express';
 import Connection from './database/db.js';
 import dotenv from 'dotenv';
@@ -7,12 +11,12 @@ import bodyParser from 'body-parser';
 
 const app = express();
 dotenv.config();
-app.use(bodyParser.json({extended:true}));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/',Routes);
-const PORT = 8000;
-const username =process.env.DB_USERNAME;
+app.use('/', Routes);
+const port = 8000;
+const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
-Connection(username,password);
-app.listen(PORT,()=>console.log(`server is running successfully on ${PORT}`));
+Connection(username, password);
+app.listen(process.env.PORT || port, () => console.log(`App listening on port http://localhost:${port}`))
